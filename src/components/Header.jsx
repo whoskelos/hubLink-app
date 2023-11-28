70c87329680fd89088dd000e9305e0175f6cbccb
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2023-11-28 18:28:00 +0100

    Eliminado una constante con clases para los enlaces que no se usa

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,6 @@ import { useAuth } from "../context/AuthContext";
 
 export function Header() {
     const { isAuthenticated } = useAuth();
-    const classLinks =
-        "block text-center active:text-gray-900 focus:text-gray-900 focus:font-bold py-5 focus:border-b-2 focus:border-cyan-600 hover:text-cyan-600 transition-colors";
     return (
         <nav className="bg-white flex justify-center h-[66px] border-b border-gray-300">
             <ul className="flex gap-x-4 [&>li]:text-gray-400 [&>li]:font-semibold">
