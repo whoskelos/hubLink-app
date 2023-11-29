4ecefa583b49db11dd410de83a6c86aea9a42c48
Kelvin Guerrero <kelvinguerrero2199@outlook.es>
2023-11-29 21:02:30 +0100

    User detail page creada

---
+++ src/pages/UserDetail.jsx
@@ -0,0 +1,17 @@
+
+export default function UserDetail() {
+    return (
+        <div className="py-4">
+            <header>
+                <div>
+                    <span className="text-lg text-gray-800 tracking-wide text-center lg:text-start">
+                        Detalle 
+                    </span>
+                    <h1 className="text-5xl text-gray-700 tracking-tight font-bold">
+                        Overview
+                    </h1>
+                </div>
+            </header>
+        </div>
+    )
+}
